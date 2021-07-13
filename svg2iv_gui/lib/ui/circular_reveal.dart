5d25ce2ec9e2ab0b70f9d1b2506c4d5edfa67891
Anthony Marland <amarland@dealabs.com>
2021-07-13 18:16:35 +0200

    Added `circular_reveal.dart`

---
+++ svg2iv_gui/lib/ui/circular_reveal.dart
@@ -0,0 +1,55 @@
+import 'dart:math' show sqrt, max;
+
+import 'package:flutter/material.dart';
+
+class CircularRevealAnimation extends StatelessWidget {
+  final Animation<double> animation;
+  final Offset? center;
+  final Widget child;
+
+  CircularRevealAnimation({
+    required this.animation,
+    this.center,
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: animation,
+      builder: (BuildContext _, Widget? __) {
+        return ClipPath(
+          clipper: _CircularRevealClipper(
+            progress: animation.value,
+            center: center,
+          ),
+          child: child,
+        );
+      },
+    );
+  }
+}
+
+class _CircularRevealClipper extends CustomClipper<Path> {
+  _CircularRevealClipper({required this.progress, this.center});
+
+  final double progress;
+  final Offset? center;
+
+  @override
+  Path getClip(Size size) {
+    final offset = center ?? Offset(size.width / 2, size.height / 2);
+    final maxRadius = _computeMaxRadius(size, offset);
+    return Path()
+      ..addOval(Rect.fromCircle(center: offset, radius: maxRadius * progress));
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => true;
+
+  static double _computeMaxRadius(Size size, Offset center) {
+    final width = max(center.dx, size.width - center.dx);
+    final height = max(center.dy, size.height - center.dy);
+    return sqrt(width * width + height * height);
+  }
+}
--- svg2iv_gui/lib/ui/main_window.dart
+++ svg2iv_gui/lib/ui/main_window.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:svg2iv_gui/ui/checkerboard.dart';
+import 'package:svg2iv_gui/ui/circular_reveal.dart';
 import 'package:svg2iv_gui/ui/file_system_entity_selection_field.dart';
 import 'package:svg2iv_gui/ui/file_system_entity_selection_mode.dart';
 import 'package:svg2iv_gui/ui/preview_selection_button.dart';
@@ -14,12 +15,16 @@ class App extends StatelessWidget {
       home: const MainPage(),
       title: 'svg2iv',
       theme: ThemeData(
-        primaryColor: _android_blue,
-        accentColor: _android_green,
+        colorScheme: ColorScheme.light(
+          primary: _android_blue,
+          secondary: _android_green,
+        ),
       ),
       darkTheme: ThemeData(
-        primaryColor: _android_green,
-        accentColor: _android_blue,
+        colorScheme: ColorScheme.dark(
+          primary: _android_green,
+          secondary: _android_blue,
+        ),
       ),
       debugShowCheckedModeBanner: false,
     );
@@ -33,115 +38,134 @@ class MainPage extends StatefulWidget {
   _MainPageState createState() => _MainPageState();
 }
 
-class _MainPageState extends State<MainPage> {
+class _MainPageState extends State<MainPage> with SingleTickerProviderStateMixin<MainPage> {
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 300),
+    );
+    _animation = CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.fastOutSlowIn,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('SVG to ImageVector conversion tool'),
-        actions: [
-          IconButton(
-            onPressed: () {
-              /* TODO */
-            },
-            icon: Icon(Icons.dark_mode_outlined),
-          ),
-          IconButton(
-            onPressed: () {
-              /* TODO */
-            },
-            icon: Icon(Icons.info_outlined),
-          ),
-        ],
-      ),
-      body: Row(
-        children: [
-          Expanded(
-            flex: 2,
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  FileSystemEntitySelectionField(
-                    selectionMode: FileSystemEntitySelectionMode.source_files,
-                  ),
-                  Row(
-                    children: [
-                      Checkbox(
-                        value: false,
-                        onChanged: (value) {
+    return CircularRevealAnimation(
+      animation: _animation,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('SVG to ImageVector conversion tool'),
+          actions: [
+            IconButton(
+              onPressed: () {
+                /* TODO */
+              },
+              icon: Icon(Icons.dark_mode_outlined),
+            ),
+            IconButton(
+              onPressed: () {
+                /* TODO */
+              },
+              icon: Icon(Icons.info_outlined),
+            ),
+          ],
+        ),
+        body: Row(
+          children: [
+            Expanded(
+              flex: 2,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    FileSystemEntitySelectionField(
+                      selectionMode: FileSystemEntitySelectionMode.source_files,
+                    ),
+                    Row(
+                      children: [
+                        Checkbox(
+                          value: false,
+                          onChanged: (value) {
+                            /* TODO */
+                          },
+                        ),
+                        SizedBox(width: 8.0),
+                        Text('Generate all assets in a single file'),
+                      ],
+                    ),
+                    FileSystemEntitySelectionField(
+                      selectionMode:
+                          FileSystemEntitySelectionMode.destination_directory,
+                    ),
+                    SizedBox(height: 8.0),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Extension receiver (optional)',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  top: 16.0,
+                  right: 16.0,
+                  bottom: 16.0,
+                ),
+                child: Stack(
+                  alignment: AlignmentDirectional.center,
+                  children: [
+                    FractionallySizedBox(
+                      widthFactor: 0.65,
+                      child: AspectRatio(
+                        aspectRatio: 1.0,
+                        child: Checkerboard(
+                          child: Container(color: Colors.amber.withOpacity(0.5)),
+                        ),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: PreviewSelectionButton(
+                        onPressed: () {
                           /* TODO */
                         },
-                      ),
-                      SizedBox(width: 8.0),
-                      Text('Generate all assets in a single file'),
-                    ],
-                  ),
-                  FileSystemEntitySelectionField(
-                    selectionMode:
-                        FileSystemEntitySelectionMode.destination_directory,
-                  ),
-                  SizedBox(height: 8.0),
-                  TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Extension receiver (optional)',
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.only(
-                top: 16.0,
-                right: 16.0,
-                bottom: 16.0,
-              ),
-              child: Stack(
-                alignment: AlignmentDirectional.center,
-                children: [
-                  FractionallySizedBox(
-                    widthFactor: 0.65,
-                    child: AspectRatio(
-                      aspectRatio: 1.0,
-                      child: Checkerboard(
-                        child: Container(color: Colors.amber.withOpacity(0.5)),
+                        iconData: Icons.keyboard_arrow_left_outlined,
                       ),
                     ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: PreviewSelectionButton(
-                      onPressed: () {
-                        /* TODO */
-                      },
-                      iconData: Icons.keyboard_arrow_left_outlined,
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: PreviewSelectionButton(
+                        onPressed: () {
+                          /* TODO */
+                        },
+                        iconData: Icons.keyboard_arrow_right_outlined,
+                      ),
                     ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: PreviewSelectionButton(
-                      onPressed: () {
-                        /* TODO */
-                      },
-                      iconData: Icons.keyboard_arrow_right_outlined,
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {
-          /* TODO */
-        },
-        icon: Icon(Icons.build_outlined),
-        label: Text('Convert', style: TextStyle(fontWeight: FontWeight.w600)),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+            /* TODO */
+          },
+          icon: Icon(Icons.build_outlined),
+          label: Text('Convert', style: TextStyle(fontWeight: FontWeight.w600)),
+        ),
       ),
     );
   }
