9cf2663ee04c270301c0311266e53eddda47621b
Anthony Marland <anthony.marland@outlook.com>
2021-09-28 20:57:14 +0200

    Reworked 'writeImageVectorsToFile() generates code that can be compiled' to work with a ZIP file

--- cli/test/destination_file_writer_test.dart
+++ cli/test/destination_file_writer_test.dart
@@ -33,7 +33,10 @@ void main() {
 path(
     fill = SolidColor(Color(0x11223344)),
 ) {
-''')..writeln(_pathDataAsString)..writeln('}')).toString();
+''')
+            ..writeln(_pathDataAsString)
+            ..writeln('}'))
+          .toString();
       final actual = StringBuffer()
           .also((buffer) => writePath(buffer, path, 0))
           .toString();
@@ -154,9 +157,8 @@ group(
 
 ''';
       final generatedSource = generatedSourceBuffer.toString();
-      final tempDirectoryPath =
-          Directory.systemTemp.path + Platform.pathSeparator;
-      final scriptSourceFile = File(tempDirectoryPath + 'test_script.main.kts')
+      final tempDirectoryPath = Directory.systemTemp.path;
+      final scriptSourceFile = File(tempDirectoryPath + '/test_script.main.kts')
         ..createSync()
         ..writeAsStringSync(dependencyAnnotations)
         ..writeAsStringSync(
@@ -168,15 +170,39 @@ group(
           mode: FileMode.append,
           flush: true,
         );
-      const workingDirectory = './test_tool/kotlinc';
-      final executable = '$workingDirectory/bin/kotlinc';
+      final workingDirectory = Directory('$tempDirectoryPath/kotlinc');
+      final workingDirectoryPath = workingDirectory.path.replaceAll('\\', '/');
+      if (!workingDirectory.existsSync() ||
+          workingDirectory
+              .listSync()
+              .where((e) => e is Directory || e is File)
+              .isEmpty) {
+        final compilerArchiveFile =
+            Directory('test_tool').listSync().singleWhere(
+          (e) {
+            // ignore: unnecessary_string_escapes
+            final fileNameRegExp = RegExp('''kotlin-compiler-.{3,6}\.zip''');
+            return fileNameRegExp.allMatches(e.path).length == 1;
+          },
+          orElse: () => fail(
+            'The archive file containing the Kotlin compiler'
+            ' could not be found!',
+          ),
+        ) as File;
+        Process.runSync(
+          'tar',
+          ['-xf', compilerArchiveFile.absolute.path],
+          workingDirectory: tempDirectoryPath,
+        );
+      }
+      final executable = '$workingDirectoryPath/bin/kotlinc';
       // use PowerShell on Windows as it understands slashes as path separators
       try {
         final result = Process.runSync(
           Platform.isWindows ? 'powershell' : executable,
           [
             '-cp',
-            '$workingDirectory/lib/kotlin-main-kts.jar',
+            '$workingDirectoryPath/lib/kotlin-main-kts.jar',
             '-script',
             scriptSourceFile.path,
           ].also((args) {
